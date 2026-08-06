537584a9dd63f39a10292058c9b6ba758fd2f929
GitHub Action <action@github.com>
2026-08-06 04:51:52 +0000

    chore: release v4.1.5

--- src/pptxgen.ts
+++ src/pptxgen.ts
@@ -105,7 +105,7 @@ import * as genTable from './gen-tables'
 import * as genXml from './gen-xml'
 import { warnDeprecatedOnce } from './gen-utils'
 
-const VERSION = '4.1.4'
+const VERSION = '4.1.5'
 
 export default class PptxGenJS implements IPresentationProps {
 	// Property getters/setters
