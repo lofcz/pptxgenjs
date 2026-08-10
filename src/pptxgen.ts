80e347172311a684231664aecd2b0bd3f597cff5
GitHub Action <action@github.com>
2026-08-10 01:50:23 +0000

    chore: release v4.1.10

--- src/pptxgen.ts
+++ src/pptxgen.ts
@@ -108,7 +108,7 @@ import * as genXml from './gen-xml'
 import * as genComments from './gen-comments'
 import { warnDeprecatedOnce } from './gen-utils'
 
-const VERSION = '4.1.9'
+const VERSION = '4.1.10'
 
 export default class PptxGenJS implements IPresentationProps {
 	// Property getters/setters
